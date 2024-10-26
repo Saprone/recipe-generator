b04efeababab898868f15e1fa1dc58db1c2088f8
Bas <onrust.bas@gmail.com>
2024-10-26 16:02:39 +0200

    added IngredientServiceTest template

---
+++ src/test/java/com/saprone/food_recipe_generator/service/IngredientServiceTest.java
@@ -0,0 +1,16 @@
+package com.saprone.food_recipe_generator.service;
+
+import org.junit.jupiter.api.Test;
+
+class IngredientServiceTest {
+
+    @Test
+    public void fetchAndSaveIngredients() {
+        System.out.println("Test method: fetchAndSaveIngredients.");
+    }
+
+    @Test
+    public void getAllIngredients() {
+        System.out.println("Test method: getAllIngredients.");
+    }
+}
