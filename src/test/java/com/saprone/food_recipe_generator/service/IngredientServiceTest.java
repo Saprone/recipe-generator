176bae672714f7c972bf8034224bdab6b7d6d4ae
Bas <onrust.bas@gmail.com>
2024-10-26 17:09:46 +0200

    added testGetAllIngredients() unit test

--- src/test/java/com/saprone/food_recipe_generator/service/IngredientServiceTest.java
+++ src/test/java/com/saprone/food_recipe_generator/service/IngredientServiceTest.java
@@ -1,14 +1,18 @@
 package com.saprone.food_recipe_generator.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.saprone.food_recipe_generator.model.Ingredient;
 import com.saprone.food_recipe_generator.repository.IngredientRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestTemplate;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -23,6 +27,11 @@ public class IngredientServiceTest {
 
     @InjectMocks
     private IngredientService ingredientService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     public void testFetchAndSaveIngredients_whenDatabaseIsEmpty() {
@@ -48,5 +57,29 @@ public class IngredientServiceTest {
 
         // Assert
         verify(ingredientRepository, never()).save(any(Ingredient.class)); // Verify that save was never called
+    }
+
+    @Test
+    void testGetAllIngredients() {
+        // Arrange
+        List<Ingredient> mockIngredients = new ArrayList<>();
+
+        Ingredient ingredient1 = new Ingredient();
+        ingredient1.setName("Sugar");
+        mockIngredients.add(ingredient1);
+
+        Ingredient ingredient2 = new Ingredient();
+        ingredient2.setName("Salt");
+        mockIngredients.add(ingredient2);
+
+        when(ingredientRepository.findAll()).thenReturn(mockIngredients);
+
+        // Act
+        List<Ingredient> ingredients = ingredientService.getAllIngredients();
+
+        // Assert
+        assertEquals(2, ingredients.size());
+        assertEquals("Sugar", ingredients.get(0).getName());
+        assertEquals("Salt", ingredients.get(1).getName());
     }
 }
